43cd238e4d0895b7515a58879323ba91c3b295de
Akash Kumar <akash.kumar@knoldus.com>
2021-07-30 10:45:16 +0530

    Add the plugins

--- cal-impl/src/main/java/com/calImpl/Application.java
+++ cal-impl/src/main/java/com/calImpl/Application.java
@@ -8,7 +8,7 @@ public class Application {
         int operand2 = 0;
 
         //print the choices on the console
-        System.out.println("What operation? ('+', '-', '*', '/')");
+        System.out.println("Choose the operation('+', '-', '*', '/')");
         System.out.println(" Enter 1 for Addition");
         System.out.println(" Enter 2 for Subtraction");
         System.out.println(" Enter 3 for Multiplication");
@@ -16,7 +16,7 @@ public class Application {
 
         //statements to input choice
         Scanner in = new Scanner(System.in);
-        System.out.println("Print Any Number from above:\n");
+        System.out.println("Enter your choice:\n");
         choice = in.nextInt();
 
         //perform checking for valid choice and get input from the user
@@ -25,17 +25,17 @@ public class Application {
             System.out.println("Enter First Number:\n");
             operand1 = in.nextInt();
             in.nextLine();
-            System.out.println("Print Second Number:\n");
+            System.out.println("Enter Second Number:\n");
             operand2 = in.nextInt();
         }
         else {
-            System.out.println("Wrong Choice");
+            System.out.println("You have entered wrong choice");
         }
 
         //create a new object cal of the class Calculator
         Calculator cal = new Calculator(operand1,operand2,choice);
         //Print the solution return by the object cal
-        System.out.println("Solution = " + cal.solution);
+        System.out.println("Result = " + cal.solution);
 
 
     }
